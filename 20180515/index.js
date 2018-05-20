0c8014b26291ba16cac82f02cfd1dec6bc974e4c
guoyan <guoy@ict.ac.cn>
2018-05-20 18:04:55 +0800

    add qa.md

--- 20180515/index.js
+++ 20180515/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
 class HelloComponent extends React.Component{
     render(){
       return (

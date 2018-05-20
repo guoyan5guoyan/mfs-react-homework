6a6c48cc71df42a30dbe113096e09ddf51f083e9
guoyan <guoy@ict.ac.cn>
2018-05-20 16:13:42 +0800

    react-app-render

---
+++ 20180515/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+class HelloComponent extends React.Component{
+    render(){
+      return (
+        <div>hello world</div>
+      )
+    }
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+registerServiceWorker();
